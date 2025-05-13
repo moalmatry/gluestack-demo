2b5cf3d9e63bfef0e0f4ac56cef709f0fee06cc1
Mohamed Almatry <moalmatry2000@gmail.com>
2025-05-13 13:03:32 +0300

    feat: device-info is working

--- app/(tabs)/index.tsx
+++ app/(tabs)/index.tsx
@@ -4,9 +4,26 @@ import Header from "@/components/Header";
 import MyLimits from "@/components/MyLimits";
 import SliderBox from "@/components/SliderBox";
 import { Box } from "@/components/ui/box";
-import React from "react";
-import { SafeAreaView, ScrollView } from "react-native";
+import React, { useEffect } from "react";
+import { Platform, SafeAreaView, ScrollView } from "react-native";
+import VersionInfo from "react-native-version-info";
+import DeviceInfo from "react-native-device-info";
+
 export default function HomeScreen() {
+  useEffect(() => {
+    if (Platform.OS === "ios") {
+      //checkisUpdateNeeded()
+    } else {
+      const version = DeviceInfo.getVersion().replace(/\./g, ",");
+
+      // const version = VersionInfo.appVersion.replace(/\./g, ",");
+      // const build = VersionInfo.buildVersion;
+      const build = DeviceInfo.getBuildNumber();
+      const val = version + "," + build;
+      //const androidAppURL = 'https://play.google.com/store/apps/details?id=com.almulla.almulla.almullafinance'
+      console.log(val);
+    }
+  }, []);
   return (
     <SafeAreaView className="flex-1 bg-white android:pt-[15%]">
       <Box className="bg-[#057dcd] absolute left-0 top-0 right-0 h-[32%]" />
